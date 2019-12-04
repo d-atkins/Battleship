905e658f0e60c05659c2c4de124c0043d9cda339
David Atkins <atki1080@gmail.com>
2019-12-03 17:24:51 -0700

    Pass render test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -6,19 +6,16 @@ require_relative '../lib/ship'
 class CellTest < Minitest::Test
 
   def test_it_exists
-    # skip
     cell = Cell.new("B4")
     assert_instance_of Cell, cell
   end
 
   def test_what_ship_is_there
-    # skip
     cell = Cell.new("B4")
     assert_nil cell.ship
   end
 
   def test_coordinate_name
-    # skip
     cell = Cell.new("B4")
     assert_equal "B4", cell.coordinate
   end
@@ -52,5 +49,37 @@ class CellTest < Minitest::Test
     assert_equal 2, cell.ship.health
     assert_equal true, cell.fired_upon?
   end
+
+  def test_empty_cell_can_render
+    cell_1 = Cell.new("B4")
+    assert_equal ".", cell_1.render
+    cell_1.fire_upon
+    assert_equal "M", cell_1.render
+  end
+
+  def test_cell_with_ship_can_render
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    assert_equal ".", cell_2.render
+    assert_equal "S", cell_2.render(true)
+    assert_equal ".", cell_2.render
+    cell_2.fire_upon
+    assert_equal "H", cell_2.render
+    assert_equal "H", cell_2.render(true)
+  end
+
+  def test_can_it_be_sunk
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+    cell_2.place_ship(cruiser)
+    cell_2.fire_upon
+    assert_equal false, cruiser.sunk?
+    cruiser.hit
+    cruiser.hit
+    assert_equal true, cruiser.sunk?
+    assert_equal "X", cell_2.render
+  end
+
 
 end
