d01cb38e3109eaa0f76172e0b67ff6736077f03b
Kevin McGrevey <niv3kmcg@gmail.com>
2019-12-04 15:12:54 -0700

    Sets up and tests board

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,7 +1,27 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/board'
+require_relative '../lib/board'
+require_relative '../lib/cell'
 
 class BoardTest < Minitest::Test
 
+  def test_it_exists
+    board = Board.new
+    assert_instance_of Board, board
+  end
+
+  def test_it_has_required_number_of_cells
+    board = Board.new
+    assert_equal 16, board.cells.count
+  end
+
+  def test_cells_are_a_hash
+   board = Board.new
+   assert_instance_of Hash, board.cells
+ end
+
+ def test_key_points_to_cell_object
+  board = Board.new
+  assert_instance_of Cell, board.cells.first[1]
+end
 end
