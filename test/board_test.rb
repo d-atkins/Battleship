b75c9113ccc5209e7f2d6a10000566271dd3e442
David Atkins <atki1080@gmail.com>
2019-12-04 15:20:45 -0700

    Test valid_coordinate?

--- test/board_test.rb
+++ test/board_test.rb
@@ -18,10 +18,19 @@ class BoardTest < Minitest::Test
   def test_cells_are_a_hash
    board = Board.new
    assert_instance_of Hash, board.cells
- end
+  end
 
- def test_key_points_to_cell_object
-  board = Board.new
-  assert_instance_of Cell, board.cells.first[1]
-end
+  def test_key_points_to_cell_object
+    board = Board.new
+    assert_instance_of Cell, board.cells.first[1]
+  end
+
+  def test_it_has_valid_coordinates
+    board = Board.new
+    assert_equal true, board.valid_coordinate?("A1")
+    assert_equal true, board.valid_coordinate?("D4")
+    assert_equal false, board.valid_coordinate?("A5")
+    assert_equal false, board.valid_coordinate?("E1")
+    assert_equal false, board.valid_coordinate?("A22")
+  end
 end
