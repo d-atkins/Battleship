5dce7ec23411d447c6bdbd2bfe916dbd0953ae50
David Atkins <atki1080@gmail.com>
2019-12-09 15:49:39 -0700

    Test receive_fire and send_fire

--- test/human_player_test.rb
+++ test/human_player_test.rb
@@ -5,7 +5,22 @@ require_relative '../lib/human_player'
 class HumanPlayerTest < Minitest::Test
 
   def test_things
+    skip
     human = HumanPlayer.new
     human.get_ready
+  end
+
+  def test_send_fire_gives_valid_coordinate
+    human = HumanPlayer.new
+    my_coordinate = human.send_fire
+    assert_equal true, human.board.valid_coordinate?(my_coordinate)
+  end
+
+  def test_it_can_receive_fire
+    human = HumanPlayer.new
+    assert_equal false, human.board.cells["B2"].fired_upon?
+    human.receive_fire("B2")
+    assert_equal true, human.board.cells["B2"].fired_upon?
+    assert_equal "M", human.board.cells["B2"].render
   end
 end
