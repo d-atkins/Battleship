9e49c49b16f73b62ed5448413131697bd90f9297
David Atkins <atki1080@gmail.com>
2019-12-10 11:32:39 -0700

    Add main menu method and set up loop for game flow

--- lib/game.rb
+++ lib/game.rb
@@ -7,8 +7,16 @@ class Game
   end
 
   def set_up
-    @computer.get_ready
     @human.get_ready
+    @computer.get_ready
+  end
+
+  def main_menu
+    system("clear")
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit."
+    user_input = gets.chomp
+    user_input.downcase
   end
 
   def take_turn(attacker, defender)
@@ -18,22 +26,34 @@ class Game
 
   def game_over?
     if @human.ships.all?{ |ship| ship.sunk?}
+      print_boards
       puts "I won!"
+      @human.press_enter_to_continue
       return true
     end
     if @computer.ships.all? { |ship| ship.sunk? }
+      print_boards
       puts "You won!"
+      @human.press_enter_to_continue
       return true
     end
     false
   end
 
   def run_game
-    set_up
-    until game_over?
-      take_turn(@human, @computer)
-      break if game_over?
-      take_turn(@computer, @human)
+    choice = main_menu
+    until (choice == "q")
+      if choice == "p"
+        set_up
+        until game_over?
+          take_turn(@human, @computer)
+          break if game_over?
+          take_turn(@computer, @human)
+        end
+        choice = main_menu
+      else
+        choice = main_menu
+      end
     end
   end
 
