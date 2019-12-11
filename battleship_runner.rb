9bef7e840d6727a1392bfca3df75774a79eac134
David Atkins <atki1080@gmail.com>
2019-12-11 14:42:13 -0700

    Add for choice between Human vs. CPU or CPU vs. CPU (WIP)

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,4 +1,12 @@
 require "./lib/game"
+require "./lib/game_computers"
 
-game = Game.new
-game.run
+puts "Press 'H' to play, press 'C' to watch CPU vs CPU"
+choice = gets.chomp.capitalize
+if choice == 'H'
+  game = Game.new
+  game.run
+elsif choice == 'C'
+  game2 = GameComputers.new
+  game2.run
+end
