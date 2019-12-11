ecfe1e7b442bc3e6a85334c2c4acd2233236247a
David Atkins <atki1080@gmail.com>
2019-12-11 10:43:49 -0700

    Specify state of board to test generate_valid_guesses on

--- test/smart_computer_test.rb
+++ test/smart_computer_test.rb
@@ -9,21 +9,23 @@ require_relative '../lib/board'
 class SmartComputerTest < Minitest::Test
 
   def setup
-    cruiser1 = Ship.new("Cruiser", 3)
-    submarine1 = Ship.new("Submarine", 2)
-    ships1 = [cruiser1, submarine1]
-    cruiser2 = Ship.new("Cruiser", 3)
-    submarine2 = Ship.new("Submarine", 2)
-    ships2 = [cruiser2, submarine2]
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 3)
+    ships = [cruiser, submarine]
     board = Board.new
-    @smart_cpu = SmartComputer.new(board, ships1, ships2)
+
+    @smart_cpu = SmartComputer.new(board, ships)
   end
 
-  def test_it_exists
-    @smart_cpu.board.cells["A1"].fire_upon
-    @smart_cpu.board.cells["B1"].fire_upon
-    @smart_cpu.board.cells["B3"].fire_upon
-    @smart_cpu.board.cells["D4"].fire_upon
+  def test_it
+    @smart_cpu.opponent_board.cells["A1"].fire_upon
+    @smart_cpu.opponent_board.cells["B1"].fire_upon
+    @smart_cpu.opponent_board.cells["B3"].fire_upon
+    @smart_cpu.opponent_board.cells["D4"].fire_upon
+    @smart_cpu.opponent_board.cells["C1"].fire_upon
+    @smart_cpu.opponent_board.cells["D2"].fire_upon
+    @smart_cpu.opponent_board.cells["B2"].fire_upon
+
     @smart_cpu.generate_valid_guesses
   end
 
